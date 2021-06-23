9c55a6990a05c330e0f8e72b42fbeef108e57f43
Bjorn Sveia <bjorn@radalabs.net>
2021-06-23 13:54:32 +0200

    Update

--- app.js
+++ app.js
@@ -1,84 +1,81 @@
 /*******************************************************************
  *                        Model
  ********************************************************************/
- const morseCode = {
-    "A": ".-",
-    "B": "-...",
-    "C": "-.-.",
-    "D": "-..",
-    "E": ".",
-    "F": "..-.",
-    "G": "--.",
-    "H": "....",
-    "I": "..",
-    "J": ".---",
-    "K": "-.-",
-    "L": ".-..",
-    "M": "--",
-    "N": "-.",
-    "O": "---",
-    "P": ".--.",
-    "Q": "--.-",
-    "R": ".-.",
-    "S": "...",
-    "T": "-",
-    "U": "..-",
-    "W": ".--",
-    "X": "-..-",
-    "Y": "-.--",
-    "Z": "--.."
- }
+const morseCode = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+};
 /*******************************************************************
  *                        View
  ********************************************************************/
 // main page
- const main = document.createElement("div")
- main.classList.add("mainPage")
- 
-//
-// header 
-const h1 = document.createElement("h1")
-h1.innerText= "Morse Code"
-//
+const main = document.createElement("div");
+main.classList.add("mainPage");
+// header
+const h1 = document.createElement("h1");
+h1.innerText = "Morse Code";
 // Outout Field
-const outPutHeader = document.createElement("h2")
-outPutHeader.innerText = "Translate to morse code here"
-const outPutArr = document.createElement("p")
-outPutArr.innerText = ""
+const outPutHeader = document.createElement("h2");
+outPutHeader.innerText = "Translate to morse code here";
+const outPutArr = document.createElement("p");
+outPutArr.innerText = "";
 // Input Field
-const inputF = document.createElement("input")
-inputF.setAttribute("type", "text")
-inputF.setAttribute("onchange", "changeText(this)")
-inputF.innerText = "click"
-inputF.classList.add("inputF")
+const inputF = document.createElement("input");
+inputF.setAttribute("type", "text");
+inputF.setAttribute("onchange", "changeText(this)");
+inputF.innerText = "click";
+inputF.classList.add("inputF");
 //
 // Render
 app();
 function app() {
-    const app = document.querySelector("#app")
-    app.append(main)
-    main.append(h1)
-    main.append(outPutHeader)
-    main.append(outPutArr)
-    main.append(inputF)
-    
+  const app = document.querySelector("#app");
+  app.append(main);
+  main.append(h1);
+  main.append(outPutHeader);
+  main.append(outPutArr);
+  main.append(inputF);
 }
 
 /*********************************************************************
  *                      Controller
  ********************************************************************/
-function changeText({value}) {
-    const arr = [...value];
-    const newArr= []
-    let tempVal =""
-        for(val of arr) {
-            for(const key in morseCode) {
-           key.toLowerCase() === val? (newArr.push(`${val.toUpperCase()}: <b>${morseCode[key]}</b></br>`), tempVal += `${morseCode[key]} `) : null
-          
-        }
+function changeText({ value }) {
+  const arr = [...value];
+  const newArr = [];
+  let tempVal = "";
+  for (val of arr) {
+    for (const key in morseCode) {
+      key.toLowerCase() === val
+        ? (newArr.push(`${val.toUpperCase()}: <b>${morseCode[key]}</b></br>`),
+          (tempVal += `${morseCode[key]} `))
+        : null;
     }
-    console.log(arr)
-     outPutArr.innerHTML = newArr.join('')
-     outPutHeader.innerText = tempVal
-     app()
+  }
+  outPutArr.innerHTML = newArr.join("");
+  outPutHeader.innerText = tempVal;
+  app();
 }
